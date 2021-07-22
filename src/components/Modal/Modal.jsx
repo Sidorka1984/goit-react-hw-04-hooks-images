ce3e37670864b674797a77d86f84f4625e60bd2e
Olga Ulyanchyk <sidorka.olka@gmail.com>
2021-07-22 14:14:47 +0300

    изменила модалку через createPortal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,6 +1,10 @@
 import { useEffect } from "react";
+import { createPortal } from 'react-dom';
 // import PropTypes from "prop-types";
 import styles from './Modal.module.css';
+
+
+const ModalRoot = document.querySelector('#root')
 
 export default function Modal({ onClose, children }) {
   useEffect(() => {
@@ -20,63 +24,11 @@ export default function Modal({ onClose, children }) {
     }
   };
 
-  return (
+  return createPortal(
     <div className={styles.Overlay} onClick={handleBackdropClick}>
       <div className={styles.Modal}>{children}</div>
-    </div>
+    </div>,
+    ModalRoot,
   )
 }
 
-// через createPortal выдает ошибку: Error: Target container is not a DOM element
-// import React, { Component } from 'react';
-// import { createPortal } from 'react-dom';
-// import PropTypes from 'prop-types';
-// import styles from './Modal.module.css';
-// 
-// 
-// const modalRoot = document.querySelector('#modal-root');
-// 
-// export default func Modal() {
-// useEffect(() => {
-  // window.addEventListener('keydown', closeModalEsc)
-  // return () => {
-    // window.removeEventListener('keydown', closeModalEsc)
-  // }
-// })
-  // const closeModalEsc = (e)=> {
-    // if (e.code === 'Escape') {
-      // console.log('tach ESC, close modal');
-      // onClose();
-    // }
-  // };
-// 
-  // const heandleBackDropClick = event => {
-    // console.log('click on backdrop');
-    // console.log('currentTarget:', event.currentTarget);
-    // console.log('target', event.target);
-    // if (event.currentTarget === event.target) {
-      // onClose();
-    // }
-  // };
-// 
-
-    // return createPortal(
-      // <div className={styles.Overlay} onClick={heandleBackDropClick}>
-        // {/* <div className={styles.Modal}>{ children }</div>  */}
-      // {/* </div>,  */}
-      // modalRoot
-    // )
-// 
-
-// }
-// 
-// Modal.defaultProps = {
-  // onClose: () => null,
-  // children: null,
-// }
-// 
-// Modal.propTypes = {
-  // onClose: PropTypes.func,
-  // children: PropTypes.node,
-// }
-// 
